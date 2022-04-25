5e94c13d92d1aa7e1672139d4f44f801819b2a59
tyron <tyron.nganga@student.moringaschool.com>
2022-04-25 14:45:46 +0300

    addded short codes

--- run.py
+++ run.py
@@ -30,42 +30,78 @@ def create_credentials(app_name,username,password):
 
 def save_credentials(credentials):
     '''saving credentials'''
-    Credentials.save_credentials()    
+    credentials.save_credentials()   
 
 def delete_credentials(credentials):
     '''deleting credentials'''
-    Credentials.delete_credentials()      
+    credentials.delete_credentials()      
 
-def display_credentials(cls):
+def display_credentials():
     '''returning method for creentials'''
-    return Credentials.delete_credentials()
+    return Credentials.display_credentials()
 
-def find_credentials(credentials):
-    '''finding method for credentials'''   
-    return Credentials.find_credentials()
+# def find_credentials():
+#     '''finding method for credentials'''   
+#     return Credentials.f
 
 def main():
-     print("Hello Welcome to your passwordlocker. What is your name?")
-     user_name = input()
 
-     print(f"Hello {user_name}. what would you like to Login(LOGIN)?")
-     print('\n')
+    print("Hello Welcome to your passwordlocker. What is your name?")
 
-     while True:
+    print('Create account:')
+    print('-'*10)
+    print('Username...')
+    user_name = input()
+    print('Password...')
+    password = input()
+
+    print(f'Your account {user_name} has been successfully created!!')
+
+    print('Login:')
+    print('Username...')
+    user_name = input()
+    print('Password...')
+    password = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
         
         print("Use these short codes : cc - create a new account, dc - display credetials fc -find a credentials, ex -exit the credentials list ")
+        short_code = input()
 
-if short_code == 'cc':
-            print("LOGIN")
+        if short_code == 'cc':
+            print("Create Credentials account")
             print("-"*10)
 
             print ("user name ....")
             user_name = input()
 
-            print("password ...")
+            print("App Name ...")
+            app_name = input()
+            print("Password ...")
             password = input()
 
-        save_contacts(create_user(user_name,password)) # create and save new ser.
+            save_credentials(create_credentials(user_name,app_name,password))
             print ('\n')
-            print(f"New User {user_name} {password} created")
+            print(f"New credentials {user_name} {app_name} created")
+
+            
             print ('\n')
+
+        elif short_code == 'dc':
+            if display_credentials():
+                print("Here is a list of all your credentials")
+                print('\n')
+
+                for credentials in display_credentials():
+                    print(f"{credentials.user_name} {credentials.app_name} .....{credentials.password}")
+
+                    print('\n')
+            else:
+                print('\n')
+                print("You dont seem to have any contacts saved yet")
+                print('\n')
+if __name__ == '__main__':
+    main()
